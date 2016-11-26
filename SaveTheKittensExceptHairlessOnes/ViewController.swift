b212b644eacf0d93cdd5b3470295b788ad407f2d
halfonso03 <hector.alfonso@yahoo.com>
2016-11-26 11:34:34 -0500

    added another double

--- SaveTheKittensExceptHairlessOnes/ViewController.swift
+++ SaveTheKittensExceptHairlessOnes/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var t: Double = 100
+        var t: Double = 200
         
         // Do any additional setup after loading the view, typically from a nib.
     }
